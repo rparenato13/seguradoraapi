41c119df6c2e1ee9964ff2563759dffda066cf9b
Renato Almeida <renato.p.almeida@gmail.com>
2022-03-15 13:52:25 -0300

    feat adiciona swagger

--- src/main/java/com/renatoalmeida/seguradoraapi/resources/UserResource.java
+++ src/main/java/com/renatoalmeida/seguradoraapi/resources/UserResource.java
@@ -4,21 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renatoalmeida.seguradoraapi.domain.User;
 import com.renatoalmeida.seguradoraapi.services.UserService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value="/api")
+@Api(value="API Rest Users")
+@CrossOrigin(origins = "*")
 public class UserResource {
 	
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping("/users")
+	@ApiOperation(value="Retorna lista de Usuários")
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
