aea3740b2750930f3a16aa7c379be60b39db3576
Renato Almeida <renato.p.almeida@gmail.com>
2022-03-14 23:54:41 -0300

    feat adiciona conexao mongo

--- src/main/java/com/renatoalmeida/seguradoraapi/domain/User.java
+++ src/main/java/com/renatoalmeida/seguradoraapi/domain/User.java
@@ -3,10 +3,15 @@ package com.renatoalmeida.seguradoraapi.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String cpf;
--- src/main/java/com/renatoalmeida/seguradoraapi/resources/UserResource.java
+++ src/main/java/com/renatoalmeida/seguradoraapi/resources/UserResource.java
@@ -4,23 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renatoalmeida.seguradoraapi.domain.User;
+import com.renatoalmeida.seguradoraapi.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria", "000.000.000-00");
-		User alex = new User("2", "Alex", "111.111.111-11");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
+//		User maria = new User("1", "Maria", "000.000.000-00");
+//		User alex = new User("2", "Alex", "111.111.111-11");
+//		List<User> list = new ArrayList<>();
+//		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
