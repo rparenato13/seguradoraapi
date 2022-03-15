e37589d27d2496eb9b1b6530909572abac679583
Renato Almeida <renato.p.almeida@gmail.com>
2022-03-14 23:36:32 -0300

    feat adiciona dominio user

---
+++ src/main/java/com/renatoalmeida/seguradoraapi/domain/User.java
@@ -0,0 +1,78 @@
+package com.renatoalmeida.seguradoraapi.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String cpf;
+
+	
+	public User() {
+		 
+	}
+
+
+	public User(String id, String name, String cpf) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/renatoalmeida/seguradoraapi/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.renatoalmeida.seguradoraapi.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.renatoalmeida.seguradoraapi.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria", "000.000.000-00");
+		User alex = new User("2", "Alex", "111.111.111-11");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+}
