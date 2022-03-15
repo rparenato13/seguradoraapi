c5644c1982b32721bfbf0fa197fdd2fc753075a9
Renato Almeida <renato@Renatos-MacBook-Pro.local>
2022-03-14 21:52:56 -0300

    primeiro commit

---
+++ src/main/java/com/renatoalmeida/seguradoraapi/SeguradoraapiApplication.java
@@ -0,0 +1,13 @@
+package com.renatoalmeida.seguradoraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguradoraapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguradoraapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renatoalmeida/seguradoraapi/SeguradoraapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatoalmeida.seguradoraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguradoraapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
